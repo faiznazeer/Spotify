861eb57a45bf8dad0d97f130f64c2d7b5e9f16e5
Faiz Nazeer <faiznazeer777@gmail.com>
2024-09-10 23:46:00 +0530

    Init

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Raleway } from 'next/font/google';
+import "./globals.css";
+
+const raleway = Raleway({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-raleway',
+});
+
+
+export const metadata: Metadata = {
+  title: "Spotify Clone",
+  description: "Spotify Clone",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${raleway.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
